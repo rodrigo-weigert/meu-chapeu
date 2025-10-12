cd6bc8b00b65e4af694cec2f14247336348cf8a0
Rodrigo Weigert <rodrigo-weigert@users.noreply.github.com>
2025-10-12 19:36:39 -0300

    Stop waiting for heartbeat start before identifying

--- connect.py
+++ connect.py
@@ -1,18 +1,11 @@
 #!/usr/bin/env python3
 
-import requests
-import asyncio
-import websockets
-import json
-import random
-from urllib.parse import urlencode
-
 from client import Client
-import event
 from intents import Intent
 from config import Config
 from http_client import HttpClient
 import commands
+
 
 def main():
     config = Config()
@@ -21,5 +14,6 @@ def main():
     client = Client(http_client.get_gateway_url(), Intent.GUILD_VOICE_STATES, config.api_token)
     client.register_interaction_handler("play", lambda event: http_client.respond_interaction_with_message(event, "OK!"))
     client.start()
+
 
 main()
