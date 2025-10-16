ab9fdb2956d2f480720b0dc94e13b51eb98a1efd
Rodrigo Weigert <rodrigo-weigert@users.noreply.github.com>
2025-10-16 08:17:14 -0300

    First working version of audio streaming

--- voice_event.py
+++ voice_event.py
@@ -9,7 +9,7 @@ class VoiceOpCode(Enum):
     IDENTIFY = 0
     SELECT_PROTOCOL = 1
     READY = 2
-    HEARTBEAT = 3,
+    HEARTBEAT = 3
     SESSION_DESCRIPTION = 4
     SPEAKING = 5
     HEARTBEAT_ACK = 6
