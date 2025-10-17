440b0ad07f17baaf29c8df3c5b7758eacdd69db4
Rodrigo Weigert <rodrigo.neorooker@gmail.com>
2025-10-17 11:19:09 -0300

    Add code to check encryption

--- crypto.py
+++ crypto.py
@@ -1,6 +1,12 @@
-from nacl.bindings import crypto_aead_xchacha20poly1305_ietf_encrypt
+from nacl.bindings import crypto_aead_xchacha20poly1305_ietf_encrypt, crypto_aead_xchacha20poly1305_ietf_decrypt
 
 
-def encrypt_packet(header: bytes, payload: bytes, nonce: int, key: bytes):
+def decrypt_packet(header: bytes, ciphertext: bytes, nonce: int, key: bytes) -> bytes:
+    return crypto_aead_xchacha20poly1305_ietf_decrypt(ciphertext, header, nonce.to_bytes(24, "little"), key)
+
+
+def encrypt_packet(header: bytes, payload: bytes, nonce: int, key: bytes) -> bytes:
     encrypted = crypto_aead_xchacha20poly1305_ietf_encrypt(payload, header, nonce.to_bytes(24, "little"), key)
+    # assert len(encrypted) == len(payload) + 16
+    # assert payload == decrypt_packet(header, encrypted, nonce, key)
     return encrypted
