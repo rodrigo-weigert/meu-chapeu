3ca42145c8f6fefd20b523fceac93292800ee4e8
Rodrigo Weigert <rodrigo-weigert@users.noreply.github.com>
2025-10-17 11:19:54 -0300

    Change bold/bright formatting on logs

--- logs.py
+++ logs.py
@@ -4,6 +4,6 @@ from loguru import logger as base_logger
 base_logger.remove()
 base_logger.level("IN", no=20, color="<yellow>")
 base_logger.level("OUT", no=20, color="<cyan>")
-base_logger.add(sys.stderr, colorize=True, format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> - <level>{level}</level>: [{extra[context]}] <b>{message}</b>")
+base_logger.add(sys.stderr, colorize=True, format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> - <level>{level}</level>: <b>[{extra[context]}]</b> {message}")
 
 logger = base_logger.opt(colors=True).bind(context="DEFAULT")
