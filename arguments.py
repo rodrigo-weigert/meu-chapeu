f8021526ecdd05e7252df563cdc4be057e4a795c
Rodrigo Weigert <rodrigo.neorooker@gmail.com>
2025-11-18 07:09:21 -0300

    Only send logs to stderr
    User should redirect stderr if necessary

--- arguments.py
+++ arguments.py
@@ -1,7 +1,6 @@
 import argparse
 
 _parser = argparse.ArgumentParser()
-_parser.add_argument("-l", "--stderr-logs", action="store_true", help="enables printing logs to stderr")
 _parser.add_argument("-v", "--ydl-verbose", action="store_true", help="enables verbose yt-dlp logs")
 
 args = _parser.parse_args()
--- logs.py
+++ logs.py
@@ -1,17 +1,10 @@
 import sys
 from loguru import logger as base_logger
-from arguments import args
-
-LOG_FILE_PATH = "/tmp/meu_chapeu.log"
 
 base_logger.level("IN", no=20, color="<yellow>")
 base_logger.level("OUT", no=20, color="<cyan>")
 
 base_logger.remove()
-if args.stderr_logs:
-    base_logger.add(sys.stderr, colorize=True, format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> - <level>{level}</level>: <b>[{extra[context]}]</b> {message}")
-base_logger.add(LOG_FILE_PATH, colorize=True, format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> - <level>{level}</level>: <b>[{extra[context]}]</b> {message}")
+base_logger.add(sys.stderr, colorize=True, format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> - <level>{level}</level>: <b>[{extra[context]}]</b> {message}")
 
 logger = base_logger.opt(colors=True).bind(context="DEFAULT")
-
-print(f"Logs are being written to {LOG_FILE_PATH}", file=sys.stderr)
