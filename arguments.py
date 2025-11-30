c3920a2880e2c86d2a49580aced07f51b5dab3b9
Rodrigo Weigert <rodrigo.neorooker@gmail.com>
2025-11-30 17:32:24 -0300

    Bring back saving logs to file

--- arguments.py
+++ arguments.py
@@ -2,5 +2,6 @@ import argparse
 
 _parser = argparse.ArgumentParser()
 _parser.add_argument("-v", "--ydl-verbose", action="store_true", help="enables verbose yt-dlp logs")
+_parser.add_argument("-l", "--logfile", default="/tmp/meu-chapeu/meu-chapeu.log", help="specify log file path")
 
 args = _parser.parse_args()
